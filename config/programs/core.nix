21590f738316c6d95de3dd5eec4115a321ffad4b
Daniel <danielgreenhome@gmail.com>
2023-08-25 14:43:36 +0100

    changed konnect to bluemail

--- config/programs/core.nix
+++ config/programs/core.nix
@@ -6,14 +6,7 @@
     environment.systemPackages= with pkgs; [
       firefox
       thunderbird
-      # Kontact and dependencies
-      kontact
-      libsForQt5.kmail
-      libsForQt5.kmail-account-wizard
-      libsForQt5.kmailtransport
-      libsForQt5.kaddressbook
-      libsForQt5.korganizer
-      
+      bluemail
       spotify
       libreoffice
       zoom-us
