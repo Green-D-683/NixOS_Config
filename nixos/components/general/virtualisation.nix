2314546a41e7f027ebe719d238c15e933687da7c
Daniel Green <danielgreenhome@gmail.com>
2024-05-20 22:32:40 +0100

    Added vmware - currently not building but hopefully just a timeout error.

--- nixos/components/general/virtualisation.nix
+++ nixos/components/general/virtualisation.nix
@@ -4,6 +4,18 @@
   config = {
     virtualisation = {
       waydroid.enable = true;
+
+      vmware = {
+        host = {
+          enable = true;
+          extraPackages = with pkgs; [
+            ntfs3g
+          ];
+        };
+        # guest = {
+        #   enable = true;
+        # };
+      };
     };
     # programs = {
     #   darling.enable = true;
