707d1b2c5a9986dd8a8460daacde495e6800ffc1
Daniel Green <danielgreenhome@gmail.com>
2024-09-13 12:03:21 +0100

    Nixos-Hardware usage

--- nixos/components/specific/nvidia/offload.nix
+++ nixos/components/specific/nvidia/offload.nix
@@ -1,29 +1,7 @@
 {config, pkgs, lib, ...}:
-let
-  nvidia-offload = pkgs.writeScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
-in
 {
-  config = lib.mkIf (config.systemConfig.gpu == "nvidia") { 
-    services.xserver.videoDrivers = ["nvidia"];
+  config = lib.mkIf (config.systemConfig.gpu == "nvidia") {
     hardware.nvidia={
-      open = true;
-      modesetting.enable = true;
-      prime = lib.mkIf (config.systemConfig.optimiseFor == "laptop") {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
-        reverseSync.enable = true;
-      };
-      ## Example Specification of PCI bus IDs
-      # intelBusId = "PCI:0:2:0";
-      # nvidiaBusId = "PCI:1:0:0";
       nvidiaSettings = true;
       powerManagement = {
         enable = true;
@@ -31,6 +9,5 @@ in
       };
       package=config.boot.kernelPackages.nvidiaPackages.beta;
     };
-    environment.systemPackages = lib.mkIf config.hardware.nvidia.prime.offload.enable [ nvidia-offload ];
   };
 }
