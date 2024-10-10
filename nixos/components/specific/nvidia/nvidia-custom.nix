d9e3a5c7e0322a93f8a41a3d94d973ecbcbe9aa3
Daniel <danielgreenhome@gmail.com>
2024-10-10 16:27:12 +0100

    Nvidia Suspend then Hibernate

--- nixos/components/specific/nvidia/nvidia-custom.nix
+++ nixos/components/specific/nvidia/nvidia-custom.nix
@@ -1,6 +1,8 @@
 {config, pkgs, lib, ...}:
 {
   config = lib.mkIf (config.systemConfig.gpu == "nvidia") {
+    boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+
     hardware.nvidia={
       nvidiaSettings = true;
       powerManagement = {
@@ -8,6 +10,24 @@
         finegrained = true;
       };
       package=config.boot.kernelPackages.nvidiaPackages.beta;
+    };
+
+    # Suspend Then Hibernate
+    systemd.services = let package = config.hardware.nvidia.package; in {
+      nvidia-resume = {
+        after = ["systemd-suspend-then-hibernate.service"];
+        requiredBy = ["systemd-suspend-then-hibernate.service"];
+      };
+      nvidia-suspend-then-hibernate = {
+        description = "NVIDIA system suspend-then-hibernate actions";
+        path = [ pkgs.kbd ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${package.out}/bin/nvidia-sleep.sh 'hibernate'";
+        };
+        before = [ "systemd-suspend-then-hibernate.service" ];
+        requiredBy = [ "systemd-suspend-then-hibernate.service" ];
+      };
     };
   };
 }
