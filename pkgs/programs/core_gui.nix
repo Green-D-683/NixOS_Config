58d0af740d053a07c7afa3550217f0a463d43264
Daniel <danielgreenhome@gmail.com>
2025-03-09 03:03:16 +0000

    OBS - still need to add desktop icon

--- pkgs/programs/core_gui.nix
+++ pkgs/programs/core_gui.nix
@@ -54,6 +54,51 @@
 
   rquickshare
   audio-sharing
+
+  # OBS
+  (wrapOBS {
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi
+      obs-3d-effect
+      obs-ndi
+      waveform
+      obs-tuna
+      obs-teleport
+      #obs-hyperion
+      # FIXME droidcam-obs build is broken, fix is pending:
+      # https://github.com/NixOS/nixpkgs/pull/382559
+      # droidcam-obs
+      obs-websocket
+      obs-webkitgtk
+      obs-vkcapture
+      obs-gstreamer
+      input-overlay
+      obs-multi-rtmp
+      obs-mute-filter
+      obs-text-pthread
+      obs-source-clone
+      obs-shaderfilter
+      obs-source-record
+      obs-replay-source
+      obs-livesplit-one
+      obs-freeze-filter
+      looking-glass-obs
+      obs-vintage-filter
+      obs-scale-to-sound
+      obs-composite-blur
+      obs-command-source
+      obs-vertical-canvas
+      obs-source-switcher
+      obs-move-transition
+      obs-gradient-source
+      obs-transition-table
+      obs-rgb-levels-filter
+      advanced-scene-switcher
+    ];
+  })
 ]) ++ (with pkgs.kdePackages; [
   kio-gdrive
   kaccounts-integration
