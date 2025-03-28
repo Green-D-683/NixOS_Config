1a004aed90c5e497214fd85e48cd8b6ee0f74087
Daniel <danielgreenhome@gmail.com>
2025-03-28 01:51:10 +0000

    Improve Pi image script

--- build-pi-image.sh
+++ build-pi-image.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 nix build .#nixosConfigurations.UnknownPi4Img.config.system.build.sdImage
+
+if [ "$#" = 1 ]; then 
+    device="$1"
+    dir=$(dirname "$0")
+    path=$(realpath -e "$dir")
+    file=$(ls "$path/result/sd-image")
+    dd if="$path/result/sd-image/$file" of="$device" bs=4096 conv=fsnc status=progress
+    sudo parted "$device" resizepart 2 100%
+    sudo resize2fs "$device"2
+fi 
--- pkgs/programs/core_utils.nix
+++ pkgs/programs/core_utils.nix
@@ -17,4 +17,5 @@ with pkgs; [
   distrobox
   docker
   binutils
+  parted
 ]
