ffd1c5a366c864769d91eec6852b69d7f9d25b58
Daniel <danielgreenhome@gmail.com>
2025-03-29 19:36:58 +0000

    Remove unnecessary commented out false starts

--- nixos/components/servers/ap.nix
+++ nixos/components/servers/ap.nix
@@ -35,50 +35,5 @@
       };
       firewall.allowedUDPPorts = [ 53 67 ];
     };
-    # services.create_ap = {
-    #   enable = true;
-    #   settings = {
-    #     INTERNET_IFACE = "eth0";
-    #     WIFI_IFACE = "wlo0";
-    #     SSID = "Unknown";
-    #     PASSPHRASE = "EduroamSlow";
-    #     FREQ_BAND="5";
-    #     CHANNEL="40";
-        
-
-    #   };
-    # };
   };
 }
-
-## YAML to Copy
-# network:
-  # version: 2
-  # wifis:
-  #   NM-263d8c35-b595-47eb-9821-e30ddd1887bb:
-  #     renderer: NetworkManager
-  #     match:
-  #       name: "wlan0"
-  #     access-points:
-  #       "Unknown":
-  #         band: "5GHz"
-  #         channel: 40
-  #         auth:
-  #           key-management: "psk"
-  #           password: "EduroamSlow"
-  #         mode: "ap"
-  #         networkmanager:
-  #           uuid: "263d8c35-b595-47eb-9821-e30ddd1887bb"
-  #           name: "Hotspot"
-  #           passthrough:
-  #             connection.autoconnect-priority: "1"
-  #             connection.timestamp: "1713312859"
-  #             wifi.ap-isolation: "0"
-  #             wifi-security.group: "ccmp;"
-  #             wifi-security.pairwise: "ccmp;"
-  #             wifi-security.proto: "rsn;"
-  #             ipv6.addr-gen-mode: "default"
-  #             proxy._: ""
-  #     networkmanager:
-  #       uuid: "263d8c35-b595-47eb-9821-e30ddd1887bb"
-  #       name: "Hotspot"
