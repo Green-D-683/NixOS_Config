a9034a07e9099181f17f2925d7526c42103fa5e9
Daniel <danielgreenhome@gmail.com>
2025-09-17 21:28:22 +0100

    Consistent rebuild script for different systems

--- rebuild-local.sh
+++ rebuild-local.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-sudo nixos-rebuild switch --flake .#UnknownDevice_ux535 --option tarball-ttl 0 --show-trace --impure
+sudo nixos-rebuild switch --flake .#"$NIXOS_SYSTEM_NAME" --option tarball-ttl 0 --show-trace --impure
