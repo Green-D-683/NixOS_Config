239f4d327a17102c908659ab6b244c63ae714a83
Daniel <danielgreenhome@gmail.com>
2025-09-17 21:26:18 +0100

    Try to stop Plasma from crashing by showing album covers?

--- home/.shared/klockscreen.nix
+++ home/.shared/klockscreen.nix
@@ -9,6 +9,7 @@
       appearance ={
         wallpaper = "${pkgs.resources}/share/resources/lock.png";
         alwaysShowClock = true;
+        showMediaControls = false; # Mitigate https://github.com/NixOS/nixpkgs/issues/442630
       };
     };
     workspace.splashScreen.theme = "None";
