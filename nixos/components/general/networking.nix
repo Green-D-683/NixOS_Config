0bd74c943add013718170a4f07172fe0ee272650
Daniel <danielgreenhome@gmail.com>
2025-09-22 12:18:28 +0100

    Custom Networkmanager.ensureProfiles to fix bridge-port configuration

--- nixos/components/general/networking.nix
+++ nixos/components/general/networking.nix
@@ -1,28 +1,38 @@
-{config, lib, pkgs, ...}:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  config={
+  config = {
     # Enable networking
-    networking={
-      firewall = let ports = [
-        # Required for Hotspot functionality
-        53 67
-        # MDNS
-        5353
-        53791
+    networking = {
+      firewall =
+        let
+          ports = [
+            # Required for Hotspot functionality
+            53
+            67
+            # MDNS
+            5353
+            53791
 
-        # rQuickShare
-        49999
-      ]; in {
-        allowedTCPPorts = ports;
-        allowedUDPPorts = ports;
-      };
-      networkmanager={
+            # rQuickShare
+            49999
+          ];
+        in
+        {
+          allowedTCPPorts = ports;
+          allowedUDPPorts = ports;
+        };
+      networkmanager = {
         enable = true;
-        connectionConfig.mdns=2;
+        connectionConfig.mdns = 2;
         wifi = {
-            powersave = false;
-            backend = "wpa_supplicant";
+          powersave = false;
+          backend = "wpa_supplicant";
         };
         plugins = with pkgs; [
           networkmanager-strongswan
@@ -30,7 +40,7 @@
       };
       hostName = config.systemConfig.hostname;
     };
-    services={
+    services = {
       xl2tpd.enable = true;
       strongswan.enable = true;
       strongswan.secrets = [
@@ -56,15 +66,45 @@
       };
       resolved.enable = true;
     };
-    security.pki.certificateFiles = ["${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"];
+    security.pki.certificateFiles = [ "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
 
-    systemd.services.NetworkManager-wait-online = {
+    systemd.services = {
+      NetworkManager-wait-online = {
         description = "Network Manager Wait Online";
-        documentation = ["man:NetworkManager-wait-online.service(8)"];
-        script="${pkgs.networkmanager}/bin/nm-online -s -q";
-        serviceConfig.RemainAfterExit="yes";
-        environment.NM_ONLINE_TIMEOUT="60";
-        wantedBy=["multi-user.target"];
+        documentation = [ "man:NetworkManager-wait-online.service(8)" ];
+        script = "${pkgs.networkmanager}/bin/nm-online -s -q";
+        serviceConfig.RemainAfterExit = "yes";
+        environment.NM_ONLINE_TIMEOUT = "60";
+        wantedBy = [ "multi-user.target" ];
+      };
+      NetworkManager-ensure-profiles.script =
+        let
+          cfg = config.networking.networkmanager;
+        in
+        lib.mkIf (cfg.ensureProfiles.profiles != { }) (
+          lib.mkForce (
+            let
+              path = id: "/run/NetworkManager/system-connections/${id}.nmconnection";
+              iniText = v: ''
+              ${lib.generators.toIni v.connection}
+
+              ${lib.generators.toIni (lib.filterAttrs (k: _: k != "connection") v)}
+              '';
+              iniFile = n: v: pkgs.writeText n (iniText v);
+            in
+            (
+              ''
+              mkdir -p /run/NetworkManager/system-connections
+              ''
+              + lib.concatMapStringsSep "\n" (profile: ''
+              ${pkgs.envsubst}/bin/envsubst -i ${iniFile (lib.escapeShellArg profile.n) profile.v} > ${path (lib.escapeShellArg profile.n)}
+              '') (lib.mapAttrsToList (n: v: { inherit n v; }) cfg.ensureProfiles.profiles)
+              + ''
+              ${cfg.package}/bin/nmcli connection reload
+              ''
+            )
+          )
+        );
     };
   };
 }
