d3b98c40f77a6b60913529cc8b6e6771e4a67b03
Daniel <danielgreenhome@gmail.com>
2025-09-29 17:02:42 +0100

    Force allow suspend-then-hibernate on AC

--- nixos/components/general/base-config.nix
+++ nixos/components/general/base-config.nix
@@ -39,6 +39,7 @@
     # suspend-then-hibernate
     systemd.sleep.extraConfig = ''
       HibernateDelaySec=30m
+      HibernateOnACPower=yes
       SuspendState=mem
     '';
     systemd.services.systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
