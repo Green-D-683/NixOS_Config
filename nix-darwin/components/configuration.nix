dca47fed99d58acb72541a594018eb90a30f125a
Daniel Green <danielgreenhome@gmail.com>
2025-10-23 00:46:16 +0100

    Slightly more darwin setup

--- nix-darwin/components/configuration.nix
+++ nix-darwin/components/configuration.nix
@@ -3,11 +3,41 @@
 {
   #services.nix-daemon.enable = true;
 
-  environment.defaultPackages = with pkgs; [
-    zed-editor
-    git
-    git-crypt
-  ];
+  environment = {
+    defaultPackages = with pkgs; [
+      zed-editor
+      git
+      git-crypt
+    ];
+    shells = [
+      pkgs.bash
+    ];
+    variables = {
+      BASH_SILENCE_DEPRECATION_WARNING="1";
+    };
+    etc."bash.local".text = ''
+      # Provide a nice prompt if the terminal supports it.
+      if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
+        PROMPT_COLOR="1;31m"
+        ((UID)) && PROMPT_COLOR="1;32m"
+        if [ -n "$INSIDE_EMACS" ]; then
+          # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+          PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+        else
+          PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+        fi
+        if test "$TERM" = "xterm"; then
+          PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+        fi
+      fi
+    '';
+  };
+
+  programs.bash = {
+    enable = true;
+    #completion.enable = true; # Gives a load of errors, but seems to work without this set?
+  };
+  users.users.daniel.shell = pkgs.bash;
 
   system.stateVersion = 6;
 
